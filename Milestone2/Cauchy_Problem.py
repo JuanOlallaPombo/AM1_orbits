61324ba61a9dfa47e1e59973f7d5b99339b85c65
JuanOlallaPombo <jolallapombo@gmail.com>
2023-10-19 11:30:38 +0200

    19/10 initial

--- Milestone2/Cauchy_Problem.py
+++ Milestone2/Cauchy_Problem.py
@@ -1,4 +1,4 @@
-from Temporal_Schemes import Euler, Crank_Nicolson, RK4, Inverse_Euler
+from numpy import zeros
 
 #Cauchy Problem solving function. Takes a temporal scheme, initial conditions,
 #a derivative function (F), a time domain and a timestep as inputs.
@@ -6,17 +6,7 @@ def F_Cauchy(Temporal_Scheme, U_0, F, tf, dt):
     N=int(tf/dt)
     U = zeros((len(U_0), N))
     U[:,0] = U_0
-    if Temporal_Scheme==Euler:
-        for t in range(0,N-1):
-            U[:,t+1] = Euler(U[:,t],F,dt,t)
-    elif Temporal_Scheme==Crank_Nicolson:
-        for t in range(0,N-1):
-            U[:,t+1] = Crank_Nicolson(U[:,t],F,dt,t)
-    elif Temporal_Scheme==RK4:
-        for t in range(0,N-1):
-            U[:,t+1] = RK4(U[:,t],F,dt,t)
-    elif Temporal_Scheme==Inverse_Euler:
-        for t in range(0,N-1):
-            U[:,t+1] = Inverse_Euler(U[:,t],F,dt,t)
+    for t in range(0,N-1):
+       U[:,t+1] = Temporal_Scheme(U[:,t],F,dt,t)
     return U
 
--- Milestone2/Milestone2.py
+++ Milestone2/Milestone2.py
@@ -1,32 +1,13 @@
 from numpy import zeros, array
 import matplotlib.pyplot as plt
 from Temporal_Schemes import Euler, Crank_Nicolson, RK4, Inverse_Euler
+from Cauchy_Problem import F_Cauchy
 
 #Derivative Function for Kepler's orbiting problem.
 def F_Kepler(U, t):
     x, y, vx, vy = U[0], U[1], U[2], U[3]
     mr = (x**2 + y**2)**1.5
     return array([vx, vy, -x/mr, -y/mr])
-
-#Cauchy Problem solving function. Takes a temporal scheme, initial conditions,
-#a derivative function (F), a time domain and a timestep as inputs.
-def F_Cauchy(Temporal_Scheme, U_0, F, tf, dt):
-    N=int(tf/dt)
-    U = zeros((len(U_0), N))
-    U[:,0] = U_0
-    if Temporal_Scheme==Euler:
-        for t in range(0,N-1):
-            U[:,t+1] = Euler(U[:,t],F,dt,t)
-    elif Temporal_Scheme==Crank_Nicolson:
-        for t in range(0,N-1):
-            U[:,t+1] = Crank_Nicolson(U[:,t],F,dt,t)
-    elif Temporal_Scheme==RK4:
-        for t in range(0,N-1):
-            U[:,t+1] = RK4(U[:,t],F,dt,t)
-    elif Temporal_Scheme==Inverse_Euler:
-        for t in range(0,N-1):
-            U[:,t+1] = Inverse_Euler(U[:,t],F,dt,t)
-    return U
 
 #Application to a real problem
 tf = 10                 #Time interval, here marked as final time since time starts on 0
